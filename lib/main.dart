daf2c3fe33c246e6f64c7c036d8347227543caef
JRParreno <jhonrhayparreno22@gmail.com>
2024-07-13 13:05:44 +0800

    added internet checker

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:nutri_kit/core/common/cubits/cubit/app_user_cubit.dart';
+import 'package:nutri_kit/core/config/shared_prefences_keys.dart';
+import 'package:nutri_kit/core/notifier/shared_preferences_notifier.dart';
 import 'package:nutri_kit/core/theme/theme.dart';
 import 'package:nutri_kit/dependency_injection_config.dart' as di;
 import 'package:nutri_kit/features/auth/presentation/bloc/auth_bloc.dart';
@@ -44,14 +50,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool isConnectedToInternet = false;
+  StreamSubscription? _internetConnectionStreamSubscription;
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+
   @override
   void initState() {
     super.initState();
-    context.read<AuthBloc>().add(AuthIsUserLoggedIn());
+    checkInternetConnection();
+    checkIsUserLoggedIn();
 
     Future.delayed(const Duration(seconds: 2), () {
       FlutterNativeSplash.remove();
     });
+  }
+
+  @override
+  void dispose() {
+    _internetConnectionStreamSubscription?.cancel();
+    super.dispose();
   }
 
   // This widget is the root of your application.
@@ -65,7 +83,42 @@ class _MyAppState extends State<MyApp> {
         debugShowCheckedModeBanner: false,
         theme: AppTheme.lightThemeMode,
         routerConfig: router,
+        scaffoldMessengerKey: scaffoldMessengerKey,
       ),
     );
+  }
+
+  void checkIsUserLoggedIn() {
+    // This will handle to get profile if user is logged in
+    final sharedPreferencesNotifier =
+        GetIt.instance<SharedPreferencesNotifier>();
+    final bool isLoggedIn = sharedPreferencesNotifier.getValue(
+        SharedPreferencesKeys.isLoggedIn, false);
+
+    if (isLoggedIn) {
+      context.read<AuthBloc>().add(AuthIsUserLoggedIn());
+    }
+  }
+
+  void checkInternetConnection() {
+    _internetConnectionStreamSubscription =
+        InternetConnection().onStatusChange.listen((event) {
+      switch (event) {
+        case InternetStatus.connected:
+          setState(() => isConnectedToInternet = true);
+          break;
+        case InternetStatus.disconnected:
+          setState(() => isConnectedToInternet = false);
+          break;
+        default:
+          setState(() => isConnectedToInternet = false);
+      }
+      final snackBar = SnackBar(
+        content: Text(
+            "You are ${isConnectedToInternet ? "connected" : "disconnected"} to the internet."),
+      );
+
+      scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
+    });
   }
 }
